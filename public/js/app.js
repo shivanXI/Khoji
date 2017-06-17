c84dd889bfe9bff6996a153444a5a8527aef9865
shivanXI <shivanstar7@gmail.com>
2017-06-17 12:47:27 +0530

    Added controller in routeprovider and script in main file after testing for dynammic handeling of query from customer/user.

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,11 @@
-var app = angular.module('MapApp', ['addCtrl', 'geolocation', 'gservice', 'ngRoute'])
+var app = angular.module('MapApp', ['addCtrl', 'queryCtrl', 'geolocation',  'gservice', 'ngRoute'])
 	
 	.config(function($routeProvider){
 		$routeProvider.when('/join', {
 			controller: 'addCtrl',
 			templateUrl: 'partials/addForm.html',
 		}).when('/find', {
+			controller: 'queryCtrl',
 			templateUrl: 'partials/queryForm.html',
 		}).otherwise({redirectTo:'/join'})
 	});
