8c4f1815d452df53f88d7867b89e375be006e6be
shivanXI <shivanstar7@gmail.com>
2017-06-17 11:02:09 +0530

    Added finder window/panel for specific user/customer

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
-var app = angular.module('MapApp', ['addCtrl', 'geolocation', 'gservice']);
+var app = angular.module('MapApp', ['addCtrl', 'geolocation', 'gservice', 'ngRoute'])
+	
+	.config(function($routeProvider){
+		$routeProvider.when('/join', {
+			controller: 'addCtrl',
+			templateUrl: 'partials/addForm.html',
+		}).when('/find', {
+			templateUrl: 'partials/queryForm.html',
+		}).otherwise({redirectTo:'/join'})
+	});
