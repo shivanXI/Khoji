d110bdba6baa6925c01aee1e70b67f533cb6e798
shivanXI <shivanstar7@gmail.com>
2017-06-19 11:22:59 +0530

    Added callback function for identifying categories of file in the open folder

--- SmartLens/app.js
+++ SmartLens/app.js
@@ -1,22 +1,31 @@
 'use strict';
 
-function openFolderDialog (){
+function openFolderDialog (fileHandlecb){
 	var inputField = document.querySelector('#folderSelector');
 	inputField.addEventListener('change', function () {
 		var folderPath = this.value;
-		alert('Folder path is' + folderPath);
+		//alert('Folder path is' + folderPath);
+		fileHandlecb(folderPath); //fileHandlecb is a callback
 	}, false);
 	inputField.click();
 }
 
-function bindSelectFolderClick (){
+function bindSelectFolderClick (fileHandlecb){
 	var button = document.querySelector('#select_folder');
 	button.addEventListener('click', function () {
 		//alert('clicked on the button');
-		openFolderDialog();
+		openFolderDialog(fileHandlecb);
 	});
 }
 
-window.onload = function (){
-	bindSelectFolderClick();
+function hideSelectFolderButton (){
+	var button  = document.querySelector('#select_folder');
+	button.style.display = 'none';
 }
+
+window.onload = function (){
+	bindSelectFolderClick(function (folderPath){
+		hideSelectFolderButton();
+	});
+	//Add the function soon to complete the heirarchy of function coupling.
+};
