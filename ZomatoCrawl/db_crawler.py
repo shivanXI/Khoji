0b51cfd1d7ff669cc8d144a12cd337e853666114
shivanXI <shivanstar7@gmail.com>
2017-06-24 10:24:37 +0530

    Added Setup regarding Zomato review Crawler for Adda section regarding places to visit

---
+++ ZomatoCrawl/db_crawler.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
+
+import signal
+import time
+import sys
+import os
+import csv
+
+
+chrome_driver = '' #chromedriver for chrome users
+
+def main_program():
+	global country, city, name_of_city, link, value_of_city, driver, chrome_driver
+
+
+main_program()
