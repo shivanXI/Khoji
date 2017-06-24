5eb4f80642a0e13a41030cac40b42974ff977cd4
shivanXI <shivanstar7@gmail.com>
2017-06-24 10:31:33 +0530

    Added driver for chrome users while using selenium

--- ZomatoCrawl/db_crawler.py
+++ ZomatoCrawl/db_crawler.py
@@ -1,3 +1,5 @@
+#Only for Zomato Database like restaurants servings, locations in Delhi-NCR
+
 from selenium import webdriver
 
 from selenium.webdriver.common.by import By 
@@ -12,10 +14,12 @@ import os
 import csv
 
 
-chrome_driver = '' #chromedriver for chrome users
+chrome_driver = '' #chrome_driver for chrome users
 
 def main_program():
 	global country, city, name_of_city, link, value_of_city, driver, chrome_driver
-
+	country = city = name_of_city = link = ''
+	city_val = 0
+	driver = webdriver.Chrome(chrome_driver)
 
 main_program()
