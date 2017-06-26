c790073969fa87a59ec422ada57e1c44dd9befaf
shivanXI <shivanstar7@gmail.com>
2017-06-26 17:42:39 +0530

    Loaded data of the cafe to be reviewed

--- ZomatoCrawl/review_crawler.py
+++ ZomatoCrawl/review_crawler.py
@@ -42,5 +42,31 @@ for j in xrange(12, len(cafe_in_selected_city_details)):
 		cafe_ratings = cafe_ratings.text
 	except:
 		continue
-			
 
+	try:
+		location = driver.find_element_by_class_name("resmap-img")
+		loc = location.get_attribute("style")
+		loc = loc.split('|')
+		loc = loc[2]
+	except:
+		continue		
+
+	try:
+		review_type = driver.find_element_by_xpath("//span[@class='grey-text']")
+		#print 'All',review_type[1].text
+	except:
+		continue
+
+	t = int(str(load_element.text.split()[-1]))
+	loaded = t
+
+	while load_element.is_displayed:
+		load_element.click()
+		print "Data Loaded ",loaded
+		try:
+			load_element  = driver.find_element_by_xpath("//div[@class='load-more']")
+		except:
+			break
+
+		loaded = loaded - 5	
+		
