2903a2fea68739e2c87cad0ba23e72f65883197a
shivanXI <shivanstar7@gmail.com>
2017-06-30 11:28:45 +0530

    Added clientid and server token in uber.js

--- Invite/app/invitation/js/uber.js
+++ Invite/app/invitation/js/uber.js
@@ -1,7 +1,9 @@
+var uberClientId = "YOUR_CLIENT_ID", uberServerToken = "YOUR_SERVER_TOKEN";
+
 var userLatitude, userLongitude, partyLatitude, partyLongitude;
 
 navigator.geolocation.watchPosition(function(position){
-
+	//for GPS location
 	console.log(position);
 
 	userLatitude = position.coords.latitude;
