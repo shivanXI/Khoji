acfeefc213346b364fef8b4e61d2cd222d464ce6
shivanXI <shivanstar7@gmail.com>
2017-06-30 11:37:24 +0530

    Added request through ajax in uber.js

--- Invite/app/invitation/js/uber.js
+++ Invite/app/invitation/js/uber.js
@@ -1,6 +1,10 @@
-var uberClientId = "YOUR_CLIENT_ID", uberServerToken = "YOUR_SERVER_TOKEN";
+var uberClientId = "YOUR_CLIENT_ID", 
+	uberServerToken = "YOUR_SERVER_TOKEN";
 
-var userLatitude, userLongitude, partyLatitude, partyLongitude;
+var userLatitude, 
+	userLongitude, 
+	partyLatitude, 
+	partyLongitude;
 
 navigator.geolocation.watchPosition(function(position){
 	//for GPS location
@@ -8,4 +12,26 @@ navigator.geolocation.watchPosition(function(position){
 
 	userLatitude = position.coords.latitude;
 	userLongitude = position.coords.longitude;
+
+	//Querying Uber API
+	getEstimatesForUserLocation(userLatitude, userLongitude);
 });
+
+function getEstimatesForUserLocation(latitude, longitude){
+	$.ajax({
+		url: "https://api.uber.com/v1/estimates/price",
+		headers: {
+			Authorization: "Token" uberServerToken
+		},
+		data: {
+			start_latitude: latitude,
+			start_longitude: longitude,
+			end_latitude: partyLatitude,
+			end_longitude: partyLongitude
+		},
+		success: function(result){
+			console.log(result);
+		}
+	});
+}
+
