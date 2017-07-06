05d19187b71582ddfe07067e9ac056ddb330c87e
shivanXI <shivanstar7@gmail.com>
2017-07-06 20:12:01 +0530

    Done final changes in zomsearch.js

--- ZomatoCrawl/Review&search/zomsearch.js
+++ ZomatoCrawl/Review&search/zomsearch.js
@@ -1,18 +1,19 @@
-//026661e42cb7f09bffab8cf66dd4d043
-var zomatoKey = "your_key"
+//
+var zomatoKey = "Your_key"
 
 getCategoriesOfAllRestaurants();
 
 function getCategoriesOfAllRestaurants(){
 	$.ajax({
-		url: 'https://developers.zomato.com/api/v2.1/cities?',
+		url: 'https://developers.zomato.com/api/v2.1/cities?q=New%20Delhi&city_ids=1&count=3',
 		headers: {
-			user_key : 'your_key',
-			city_ids : '1',
-			count : '2'
+			user_key : 'Your_key',
 		},
 		success: function(result){
-			console.log(result);
+			console.log(JSON.stringify(result));
+			var data = result['location_suggestions'];
+			var first = data[0]["name"];
+			console.log(first);
 		}
 	});
 }
